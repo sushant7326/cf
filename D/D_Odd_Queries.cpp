087f039514d6fc39ace7c6d1778a3471d70083c5
Sushant Singh <111882164+sushant7326@users.noreply.github.com>
2025-08-22 06:56:29 +0530

    proper rearrange

--- D/D_Odd_Queries.cpp
+++ D/D_Odd_Queries.cpp
@@ -14,12 +14,19 @@ void solve() {
         sum_total += v[i];
     }
 
+    vector<int> pref(n);
+    pref[0] = v[0];
+    for(int i = 1; i < n; i++) {
+        pref[i] = pref[i - 1] + v[i];
+    }
+
     while(q--) {
         int l, r, k;
         cin >> l >> r >> k;
         
-        int sum = 0;
-        for (int i = l-1; i < r; i++) sum += v[i];
+
+        int sum = pref[r - 1];
+        if(l > 1) sum -= pref[l - 2];
 
         if ((sum_total - sum + (k * (r-l+1))) % 2 == 1) cout << "YES" << endl;
         else cout << "NO" << endl;
@@ -31,7 +38,6 @@ void solve() {
 int32_t main() {
     int t;
     cin >> t;
-    // t = 1;
     while(t--) solve();
     return 0;
 }
