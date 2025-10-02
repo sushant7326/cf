57bf1dec1da3f86111d58db899d743f9312f14f5
Sushant Singh <111882164+sushant7326@users.noreply.github.com>
2025-10-03 01:38:57 +0530

    leetcode 146. lru cache

---
+++ leetcode/146.lru-cache.cpp
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=146 lang=cpp
+ *
+ * [146] LRU Cache
+ */
+
+// @lc code=start
+class Node {
+public:
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
+
+    Node (int key, int val): key(key), val(val), prev(nullptr), next(nullptr) {}
+};
+
+class LRUCache {
+private:
+    int cap;
+    std::unordered_map<int, Node*> cache;
+    Node* oldest;
+    Node* latest;
+
+    void remove(Node* node) {
+        Node* prev = node->prev;
+        Node* next = node->next;
+        prev->next = next;
+        next->prev = prev;
+    }
+
+    void insert (Node* node) {
+        Node* prev = latest->prev;
+        Node* next = latest;
+        prev->next = next->prev = node;
+        node->next = next;
+        node->prev = prev;
+    }
+
+public:
+    LRUCache(int capacity): cap(capacity) {
+        oldest = new Node(0,0);
+        latest = new Node(0,0);
+        oldest->next = latest;
+        latest->prev = oldest;
+    }
+    
+    int get(int key) {
+        if (cache.find(key) != cache.end()) {
+            Node* node = cache[key];
+            remove(node);
+            insert(node);
+            return node->val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) remove(cache[key]);
+
+        Node* new_node = new Node(key, value);
+        cache[key] = new_node;
+        insert(new_node);
+
+        if(cache.size() > cap) {
+            Node* lru = oldest->next;
+            remove(lru);
+            cache.erase(lru->key);
+            delete lru;
+        }
+    }
+
+    ~LRUCache() {
+        Node* curr = oldest;
+        while (curr != nullptr) {
+            Node* next = curr->next;
+            delete curr;
+            curr = next;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+// @lc code=end
+
